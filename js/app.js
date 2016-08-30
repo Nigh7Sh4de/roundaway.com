07961f0e3079eaed634560b739747e410d6fe1ed
Dennis Plotnik <adknowledgeuser@android-74633fd597416a80.adknowledge.com>
2016-08-30 18:59:11 -0400

    Add config

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import config from './config';
 import thunk from 'redux-thunk';
 import Axios from 'axios';
 var axios = Axios.create({
     baseURL: 'http://localhost:8081',
     headers: {
-        Authorization: 'JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU3YmYxYWJkNzdiY2I2MTEwOGVlM2U4MSIsInByb2ZpbGUiOnsibmFtZSI6IkRlbm5pcyBQbG90bmlrIn0sImlhdCI6MTQ3MjE0MjAyNH0.2avZgwV8ph2ZMzc-4ZRdZR495iRTGfaGzzk3aMnHZRc'
+        Authorization: 'JWT ' + config.DEFAULT_JWT
     }
 })
 import { combineReducers, createStore, applyMiddleware } from 'redux';
---
+++ js/config.example.js
@@ -0,0 +1,7 @@
+(function() {
+    roundaway.config = {
+        FACEBOOK_CLIENT_ID      :   "000000000000000",
+        GOOGLE_CLIENT_ID        :   "00000-example.com",
+        STRIPE_PUBLISH_KEY      :   "pk_test_000000abc"
+    }
+}())
