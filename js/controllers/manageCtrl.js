cc76462cbf95d55ca5b3500d411e3ab59d52ddb8
nigh7sh4de <plotnikd@gmail.com>
2016-09-05 14:14:44 -0400

    Complete new spot page

--- js/controllers/manageCtrl.js
+++ js/controllers/manageCtrl.js
@@ -15,25 +15,38 @@
             ready: false,
             location: {
                 address: undefined,
-                coordinates: {
-                    longitude: undefined,
-                    latitude: undefined
-                }
             },
             price: {
                 perHour: undefined
             }
         };
-        $scope.skipAddress = function() {
-            $scope.newspot.ready = true;
-        }
         $scope.searchAddress = function() {
-            console.log('Searching for spot... (but not actually)');
-            $scope.newspot.location.coordinates = {
-                longitude: 12.345,
-                latitude: 21.543
-            }
-            $scope.newspot.ready = true;
+            $http.post('/api/util/location/geocode', {
+                address: $scope.newspot.search_address
+            })
+            .then(function(res) {
+                $scope.newspot.result_address = res.data.data;
+                $scope.newspot.address = $scope.newspot.result_address[0];
+            })
+            .catch(function(err) {
+                newspot.error = err;
+            })
+        }
+        $scope.createSpot = function() {
+            $scope.newspot.error = null;
+            $http.put('/api/spots', {
+                location: {
+                    address: $scope.newspot.address
+                },
+                price: $scope.newspot.price
+            })
+            .then(function(res) {
+                $scope.newspot.success = true;
+            })
+            .catch(function(err) {
+                $scope.newspot.error = err.status + ': ' + (err.data.errors || err.data)
+            })
+
         }
     }
 
